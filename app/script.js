a8ba278bb027de8c47221167790cb943ba4d88bb
CasimirG12 <casimirbomans@gmail.com>
2024-02-05 08:50:52 +0100

    tweeked the blur function

--- app/script.js
+++ app/script.js
@@ -1,18 +1,20 @@
 const showRules = () => {
     const blur = document.getElementById('blur');
     const body = document.body;
-    if(blur.style.width === '0' || blur.style.width === '0px'){
+    
+    if (blur.style.width === '' || blur.style.width === '0px' || blur.style.width === '0') {
         blur.style.width = '100vw';
         blur.style.height = '100vh';
 
         body.style.overflow = 'hidden';
 
-        blur.scrollIntoView({behavior: 'smooth', block: 'center'});
-    }else{
+        blur.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    } else {
         blur.style.width = '0px';
         blur.style.height = '0px';
 
         body.style.overflow = 'auto';
     }
+    
     console.log('button pressed');
 }
