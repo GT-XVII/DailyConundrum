109a0b59c7463257c04bc710c874a1773760316b
CasimirG12 <casimirbomans@gmail.com>
2024-02-06 17:57:46 +0100

    added new js files for better overview

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/game", methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
     return render_template("game.html")
 
