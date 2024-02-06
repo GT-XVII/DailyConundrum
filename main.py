b83e21c8174b40d3e35d2a59736522ea655e2612
Ruan Moolman-Dreyer <ruanmoolmandreyer@icloud.com>
2024-02-06 16:31:15 +0100

    Check Cas

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/index", methods=["GET"])
+@app.route("/game", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("game.html")
+
+@app.route("/scoreboard", methods=["GET"])
+def scoreboard():
+    return render_template("scoreboard.html")
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+@app.route("/contact", methods=["GET"])
+def contact():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
