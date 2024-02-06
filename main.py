3d2cb2ea2b58053c337f059b135555d8f56e1b33
Ruan Moolman-Dreyer <ruanmoolmandreyer@icloud.com>
2024-02-06 17:48:07 +0100

    Template Extensions and Login Page

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def login():
+    return render_template("login.html")
+
 @app.route("/game", methods=["GET"])
-def index():
+def game():
     return render_template("game.html")
 
 @app.route("/scoreboard", methods=["GET"])
