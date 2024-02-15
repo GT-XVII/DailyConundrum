ae516923c1a61d9e6f496b27d05fc6f7c70b8822
Ruan Moolman-Dreyer <ruanmoolmandreyer@icloud.com>
2024-02-15 14:00:02 +0100

    Account creation update

--- static/login.js
+++ static/login.js
@@ -3,10 +3,8 @@ function validateForm() {
     var inputpassword = $('#password').val();
 
     if (inputname && inputpassword) {
-        // If form validation passes, submit the form
         $('form').submit();
     } else {
-        // If validation fails, alert the user
         alert("Please enter both username and password");
     }
 }
